f96af9aca1e36e98a508951cc801995b67da7cae
encodeous <encodeous7@gmail.com>
2021-07-22 11:09:13 -0400

    Restore retention

--- src/main/java/ca/encodeous/simplenms/annotations/NMSMethod.java
+++ src/main/java/ca/encodeous/simplenms/annotations/NMSMethod.java
@@ -4,9 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface NMSMethod {
 
-    //TODO move this into repeatable annotation
     NMSVersionName[] versionNames() default {};
 }
